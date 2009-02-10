1cbaa2d1efd867b5dd2e64165003790a1e225c64
Fabien Jakimowicz <rider@soda.local>
2009-02-10 02:11:18 +0100

    adding comments

--- lib/longurl.rb
+++ lib/longurl.rb
@@ -10,6 +10,22 @@ require 'longurl/direct'
 module LongURL
   
   class << self
+    # Expand given <tt>url</tt> to a longest one.
+    # First, expand will try to expand url using longurl.org service.
+    # Then, it will try to direct follow redirections on the given url and returns final one.
+    # === Types
+    # <tt>url</tt> is expected to be a String and returns a String with the url.
+    # === Examples
+    #   # simple expands
+    #   LongURL.expand("http://tinyurl.com/1c2")    # => "http://www.google.com"
+    #   LongURL.expand("http://tinyurl.com/blnhsg") # => "http://www.google.com/search?q=number+of+horns+on+a+unicorn&ie=UTF-8"
+    #   LongURL.expand("http://is.gd/iUKg")         # => "http://fabien.jakimowicz.com"
+    #
+    #   # not expandable urls
+    #   LongURL.expand("http://www.linuxfr.org")    # => "http://www.linuxfr.org"
+    # === Exceptions
+    # * LongURL::InvalidURL : will occurs if given url is nil, empty or invalid
+    # * LongURL::UnknownError : an unknown error occurs
     def expand(url)
       @@service ||= Service.new
       @@service.query(url)
--- lib/longurl/exceptions.rb
+++ lib/longurl/exceptions.rb
@@ -1,10 +1,13 @@
 module LongURL
+  # Raised by LongURL::Service class if longurl.org service returns unsupported service error.
   class UnsupportedService < Exception
   end
 
+  # Raised by LongURL::Service class if longurl.org service returns a not supported answer.
   class UnknownError < Exception
   end
   
+  # Raised by LongURL::Service if supplied url is invalid (nil, empty, ...)
   class InvalidURL < Exception
   end
 end
