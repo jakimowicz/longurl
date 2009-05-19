f4a070025894049e464fdd1cfedbb69277c68770
Fabien Jakimowicz <fabien@jakimowicz.com>
2009-05-19 18:03:37 +0200

    adding a regexp constant to match short urls

--- lib/longurl/constants.rb
+++ lib/longurl/constants.rb
@@ -1,6 +1,9 @@
 require "uri"
 
 module LongURL
+  ShortURLMatchRegexp = /http:\/\/[\/-_.a-z0-9]+/im
+
+  # Urls for longurl
   EndPoint        = URI.parse("http://api.longurl.org/v1/expand")
   ServiceEndPoint = URI.parse("http://api.longurl.org/v1/services")
 end
